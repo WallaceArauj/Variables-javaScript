355f09b8fb615f9f2b98cf0230747772cdf656fe
WallaceArauj <wallace.hostile94@gmail.com>
2022-09-08 17:06:05 -0300

    programa

---
+++ programa.js
@@ -0,0 +1,12 @@
+const prompt = require (`prompt-sync`)();
+const userName = prompt ('Diga seu nome: ');
+console.log(`Nome: ${userName}`);
+const lastName = prompt ('Diga seu sobrenome: ');
+console.log(`Sobrenome: ${lastName}`);
+const weight = prompt ('Diga sua altura: ');
+console.log(`Altura: ${weight}`);
+const hospitalizationDate = prompt ('Data de internação: ');
+console.log(`Data de internação: ${hospitalizationDate}`);
+const diabete = prompt ('Você possui diabetes: ');
+console.log(`Diabete: ${diabete}`)
+
